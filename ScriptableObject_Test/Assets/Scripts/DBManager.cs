9c631bf45b6f37994d43df10993557e3f280dd78
Saito_Hiroki <longest3110@gmail.com>
2019-06-16 23:09:38 +0900

    [issue #1] 最初にカラム名を表示するRowを生成

--- ScriptableObject_Test/Assets/Scripts/DBManager.cs
+++ ScriptableObject_Test/Assets/Scripts/DBManager.cs
@@ -3,4 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DBManager : MonoBehaviour {
+	SqliteDatabase sqlDB;
+
+	private void Awake() {
+		sqlDB = new SqliteDatabase("newKanjiMaster.db");
+	}
+
+	public DataTable GetTable() {
+		return sqlDB.ExecuteQuery("select * from idioms_master");
+	}
 }
--- ScriptableObject_Test/Assets/Scripts/NodeGenerator.cs
+++ ScriptableObject_Test/Assets/Scripts/NodeGenerator.cs
@@ -3,4 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeGenerator : MonoBehaviour {
+
+	[SerializeField]
+	DBManager dbManager;
+
+	[SerializeField]
+	RowNode rowNode;
+
+	private void Start() {
+		DataTable data = dbManager.GetTable();
+
+		// カラム名の追加
+		RowNode columnRow = Instantiate(rowNode, rowNode.transform.parent);
+		foreach(string column in data.Columns) {
+			columnRow.AddColumnNode(column);
+		}
+		columnRow.gameObject.SetActive(true);
+
+
+		// foreach(DataRow row in data.Rows) {
+		// 	string s = "";
+		// 	foreach(string column in data.Columns) {
+		// 		s += (row[column].ToString() + ", ");
+		// 	}
+
+		// 	Debug.Log(s);
+		// }
+	}
 }
